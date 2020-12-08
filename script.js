c55b33aa3eccd47d152edff3f396696b1f7c31b0
Aitalina Kirillina <ayta1951@gmail.com>
2020-12-08 14:59:29 +0100

    added files js html

---
+++ script.js
@@ -0,0 +1,29 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions()
+
+
+
+const fivePlusSeven = () => console.log(5 + 7);
+
+fivePlusSeven();
+
+
+
+const myFunction = (a, b) => console.log(a + b); 
+
+myFunction(2, 2);
+
+
+
+const addFive = a => console.log(a + 5);
+
+addFive(6);
+
+
+
+
+const createObject = () => ({greeting: "hoi"});
+
+createObject();
